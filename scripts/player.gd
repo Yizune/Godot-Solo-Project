cfdf62938386f3d2c8169a79205ff1e7878b1088
yizune1 <132386110+Yizune@users.noreply.github.com>
2025-01-29 17:18:30 +0100

    tiles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,68 @@
 extends CharacterBody2D
 
+const speed = 100
+var current_dir = "none"
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	player_movement(delta)
+	
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		play_anim(1)
+		current_dir = "left"
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		play_anim(1)
+		current_dir = "down"
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		play_anim(1)
+		current_dir = "up"
+		velocity.x = 0
+		velocity.y = -speed
+	else :
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+		
 	move_and_slide()
+	
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("side_walk")
+		elif movement == 0:
+			anim.play("side_idle")
+			
+	if dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
+	if dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")									
