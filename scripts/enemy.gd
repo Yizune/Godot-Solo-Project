879e11bf32c8ededdcb388a49d3cc9411bd21fa1
yizune1 <132386110+Yizune@users.noreply.github.com>
2025-01-30 13:02:04 +0100

    commit

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,7 +4,14 @@ var speed = 45
 var player_chase = false
 var player = null
 
+var health = 100
+var player_in_attack_zone = false
+
+var can_take_dmg = true
+
 func _physics_process(delta: float) -> void:
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position) / speed
 		
@@ -23,3 +30,30 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+	
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_in_attack_zone = false
+
+func deal_with_damage():
+	if player_in_attack_zone == true and Global.player_current_attack == true:
+		if can_take_dmg == true:
+			health -= 20
+			$take_damage_cd.start()
+			can_take_dmg = false
+			print("slime health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_cd_timeout() -> void:
+	can_take_dmg = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var enemy_att_range = false
+var enemy_att_cd = true
+var health = 200
+var player_alive = true
+
+var attack_in_progress = false
+
 const speed = 100
 var current_dir = "none"
 
@@ -7,30 +14,42 @@ func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta: float) -> void:
+	attack()
 	player_movement(delta)
+	enemy_attack()
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		print("player is dead")
+		self.queue_free()
+		#respawn/death screen here
 	
 func player_movement(delta):
+	if attack_in_progress:  # Prevent movement while attacking
+		return 
+	
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
-		play_anim(1)
 		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
-		play_anim(1)
 		current_dir = "down"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
-		play_anim(1)
 		current_dir = "up"
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = -speed
-	else :
+	else:
 		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
@@ -41,28 +60,83 @@ func play_anim(movement):
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
+	# Prevent overriding attack animations
+	if attack_in_progress:
+		return 
+	
 	if dir == "right":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("side_walk")
-		elif movement == 0:
+		else:
 			anim.play("side_idle")
-	if dir == "left":
+	elif dir == "left":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("side_walk")
-		elif movement == 0:
+		else:
 			anim.play("side_idle")
-			
-	if dir == "down":
+	elif dir == "down":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("front_walk")
-		elif movement == 0:
+		else:
 			anim.play("front_idle")
-	if dir == "up":
+	elif dir == "up":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("back_walk")
-		elif movement == 0:
+		else:
 			anim.play("back_idle")									
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_att_range = true
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_att_range = false
+		
+func enemy_attack():
+	if enemy_att_range and enemy_att_cd:
+		health -= 20
+		enemy_att_cd = false
+		$attack_cd.start()
+		print(health)
+
+func _on_attack_cd_timeout() -> void:
+	enemy_att_cd = true
+
+func attack():
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if Input.is_action_just_pressed("attack"):
+		Global.player_current_attack = true
+		attack_in_progress = true
+		
+		# Set flip_h before playing animation
+		if dir == "right":
+			anim.flip_h = false
+			anim.play("side_attack")
+		elif dir == "left":
+			anim.flip_h = true
+			anim.play("side_attack")
+		elif dir == "down":
+			anim.flip_h = false
+			anim.play("front_attack")
+		elif dir == "up":
+			anim.flip_h = false
+			anim.play("back_attack")
+
+		print("Attacking:", dir, "Animation:", anim.animation, "Flip:", anim.flip_h)
+		
+		$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout() -> void:
+	$deal_attack_timer.stop()
+	Global.player_current_attack = false
+	attack_in_progress = false
